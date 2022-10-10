c77c2f57e44bc2766219139a646e15f7623fff2c
Adarsh Ravishankar <89323486+AdarshRavis@users.noreply.github.com>
2022-10-09 20:46:32 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from audiorecorder import audiorecorder
+import whisper
+
+model = whisper.load_model("large")
 
 st.title("Audio Recorder")
 audio = audiorecorder("Click to record", "Recording...")
@@ -7,8 +10,11 @@ audio = audiorecorder("Click to record", "Recording...")
 if len(audio) > 0:
     # To play audio in frontend:
     st.audio(audio)
-    
+
     # To save audio to a file:
     wav_file = open("audio.mp3", "wb")
     wav_file.write(audio.tobytes())
 
+
+result = model.transcribe("audio.mp3")
+print(result["text"])
